3f9b1bd7e4419d0a7a813f4a58d5d52746d92b81
gustavo_h_schneider <gustavo.schneider@me.com.br>
2021-06-25 09:13:37 -0300

    Segundo modulo

--- maior-ou-menor.rb
+++ maior-ou-menor.rb
@@ -1,11 +1,29 @@
 puts "Bem vindo ao jogo da adivinhação!!"
 puts "Qual é o seu nome?"
 nome = gets
-puts "\n\n\n Começaremos o jogo para você, " + nome
+puts "\n\n\nComeçaremos o jogo para você, " + nome
 puts "\n\nEscolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "\nEscolhido... que tal adivinhar o número secreto?"
-puts "\n\n\nTentativa 1\nEntre com o número:"
-chute = gets
-puts "\nSerá que acertou? Você informou: " +chute
-puts chute.to_i == numero_secreto
+numero_tentativas = 5
+
+for tentativa in 1..5
+    puts "\n\n\nTentativa "+tentativa.to_s+"/"+numero_tentativas.to_s
+    puts "\nEntre com o número:"
+    chute = gets
+    puts "\nSerá que acertou? Você informou: " +chute
+
+    acertou = numero_secreto == chute.to_i
+
+    if acertou
+        puts "Acertou!"
+        break
+    else
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior!"
+        else
+            puts "O número secreto é menor!"
+        end
+    end
+end
--- testes.rb
+++ testes.rb
@@ -3,3 +3,16 @@ puts 175 == 174
 puts "175" == 175
 puts "175" == "175"
 puts "175".to_i == 175
+
+##################
+puts 175 != 174
+puts 175 != 175
+puts 175 > 174
+puts 175 >= 175
+puts 175 < 176
+puts 175 <= 175
+
+for dedos in 1..3
+    puts "Contando " + dedos.to_s
+    puts "Contando #{dedos}"
+end

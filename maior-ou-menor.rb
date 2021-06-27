84f86212d476bea5f42395a18116856a99c51446
gustavo_h_schneider <gustavo.schneider@me.com.br>
2021-06-27 14:18:59 -0300

    Terceiro modulo

--- maior-ou-menor.rb
+++ maior-ou-menor.rb
@@ -1,29 +1,49 @@
-puts "Bem vindo ao jogo da adivinhação!!"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\nComeçaremos o jogo para você, " + nome
-puts "\n\nEscolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "\nEscolhido... que tal adivinhar o número secreto?"
-numero_tentativas = 5
+def da_boas_vindas
+    puts "Bem vindo ao jogo da adivinhação!!"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\nComeçaremos o jogo para você, " + nome
+end
 
-for tentativa in 1..5
-    puts "\n\n\nTentativa "+tentativa.to_s+"/"+numero_tentativas.to_s
+def sorteia_numero_secreto
+    puts "\n\nEscolhendo um número secreto entre 0 e 200..."
+    sorteado = 175
+    puts "\nEscolhido... que tal adivinhar o número secreto?"
+    sorteado
+end
+
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "\n\n\nTentativa "+tentativa.to_s+"/"+limite_de_tentativas.to_s
     puts "\nEntre com o número:"
     chute = gets
     puts "\nSerá que acertou? Você informou: " +chute
+    chute.to_i
+end
 
-    acertou = numero_secreto == chute.to_i
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
 
     if acertou
         puts "Acertou!"
-        break
-    else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior!"
-        else
-            puts "O número secreto é menor!"
-        end
+        return true
     end
+
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior!"
+    else
+         puts "O número secreto é menor!"
+    end
+    false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+(1..limite_de_tentativas).each do |tentativa|
+    chute = pede_um_numero tentativa, limite_de_tentativas
+
+    break if verifica_se_acertou numero_secreto, chute
 end
